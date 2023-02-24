f51824607f6e71bc57b11ca026296835bfd20552
Svyatoslav <Swyatoslavik161@yandex.ru>
2023-02-24 21:47:11 +0300

    123

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -8,4 +8,4 @@ from keyboards.default import kb_menu
 
 @dp.message_handler(Command("menu"))
 async def menu(message: types.Message):
-    await message.answer("Главное меню:", reply_markup=kb_menu)
+    await message.answer("🏠Главное меню:", reply_markup=kb_menu)
