7fb71055237a5b1544240b1e74f14f0a63b14899
Svyatoslav <Swyatoslavik161@yandex.ru>
2023-02-24 09:38:13 +0300

    1st commit

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -2,13 +2,12 @@ import logging
 
 from aiogram import Dispatcher
 
-from data.config import admins_id
+
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin_id in admins_id:
         try:
             text = "Бот запущен"
-            await dp.bot.send_message(admin_id, text)
+            await dp.bot.send_message(473151013, text)
         except Exception as err:
             logging.exception(err)
